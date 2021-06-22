e250c9ed8e473679e9fbebd7536a5209560cf0d2
ozodtagoev <ozod.tagoev@gmail.com>
2021-06-22 19:14:37 +0500

    Adding drawer!

--- lib/main.dart
+++ lib/main.dart
@@ -39,12 +39,86 @@ class _HomePageState extends State<HomePage> {
       drawer: Drawer(
         child: ListView(
           children: [
-            CircleAvatar(
-              backgroundImage: AssetImage('images/avatar.jpg'),
-            ),
             UserAccountsDrawerHeader(
+              currentAccountPicture: GestureDetector(
+                child: CircleAvatar(
+                  //backgroundImage: AssetImage('images/avatar.jpg'),
+                  child: Icon(Icons.person, color: Colors.white,),
+                  backgroundColor: Colors.grey,
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.redAccent,
+              ),
               accountName: Text('Ozod Tagoev'),
               accountEmail: Text('ozod.tagoev@gmail.com'),
+            ),
+            InkWell(
+              onTap: () {
+
+              },
+              child: ListTile(
+                title: Text('Home Page'),
+                leading: Icon(Icons.home),
+              ),
+            ),
+            InkWell(
+              onTap: () {
+
+              },
+              child: ListTile(
+                title: Text('My Account'),
+                leading: Icon(Icons.person),
+              ),
+            ),
+            InkWell(
+              onTap: () {
+
+              },
+              child: ListTile(
+                title: Text('My Orders'),
+                leading: Icon(Icons.shopping_basket),
+              ),
+            ),
+            InkWell(
+              onTap: () {
+
+              },
+              child: ListTile(
+                title: Text('Categories'),
+                leading: Icon(Icons.dashboard),
+              ),
+            ),
+            InkWell(
+              onTap: () {
+
+              },
+              child: ListTile(
+                title: Text('Favorites'),
+                leading: Icon(Icons.favorite),
+              ),
+            ),
+            Divider(
+              height: 50,
+              color: Colors.grey[400],
+            ),
+            InkWell(
+              onTap: () {
+
+              },
+              child: ListTile(
+                title: Text('Settings'),
+                leading: Icon(Icons.settings),
+              ),
+            ),
+            InkWell(
+              onTap: () {
+
+              },
+              child: ListTile(
+                title: Text('About'),
+                leading: Icon(Icons.help),
+              ),
             ),
           ],
         ),
