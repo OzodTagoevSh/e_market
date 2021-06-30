1972f0044467d6e20f8670133b09e8858d1eda21
ozodtagoev <ozod.tagoev@gmail.com>
2021-06-30 23:15:56 +0500

    Login with google finished.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:e_market/pages/login.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 
-void main() => runApp(MaterialApp(
-  debugShowCheckedModeBanner: false,
-  home: Login(),
-));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Login(),
+  ));
+}
 
 
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -6,7 +6,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
-
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
 
@@ -17,7 +16,7 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   final GoogleSignIn googleSignIn = new GoogleSignIn();
   final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
-  SharedPreferences ?preferences;
+  SharedPreferences? preferences;
   bool isLoggedIn = false;
   bool loading = false;
 
@@ -32,11 +31,12 @@ class _LoginState extends State<Login> {
       loading = true;
     });
 
-    preferences = await SharedPreferences.getInstance(); 
+    preferences = await SharedPreferences.getInstance();
     isLoggedIn = await googleSignIn.isSignedIn();
 
-    if(isLoggedIn) {
-      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomePage()));
+    if (isLoggedIn) {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => HomePage()));
     }
 
     setState(() {
@@ -44,9 +44,91 @@ class _LoginState extends State<Login> {
     });
   }
 
+  Future handleSignIn() async {
+    preferences = await SharedPreferences.getInstance();
+
+    setState(() {
+      loading = true;
+    });
+
+    FirebaseAuth auth = FirebaseAuth.instance;
+    GoogleSignInAccount? googleUser = await googleSignIn.signIn();
+    GoogleSignInAuthentication googleSignInAuthentication =
+        await googleUser!.authentication;
+    AuthCredential credential = GoogleAuthProvider.credential(
+        idToken: googleSignInAuthentication.idToken,
+        accessToken: googleSignInAuthentication.accessToken);
+    UserCredential authResult = await auth.signInWithCredential(credential);
+    User? user = authResult.user;
+    if (user != null) {
+      final QuerySnapshot result = await FirebaseFirestore.instance
+          .collection('users')
+          .where('id', isEqualTo: user.uid)
+          .get();
+      final List<DocumentSnapshot> documents = result.docs;
+
+      if (documents.length == 0) {
+        // Inserting the user to the collection
+        FirebaseFirestore.instance.collection('users').doc(user.uid).set({
+          'id': user.uid,
+          'username': user.displayName,
+          'profilePicture': user.photoURL,
+        });
+
+        await preferences!.setString('id', user.uid);
+        await preferences!.setString('username', user.displayName.toString());
+        await preferences!.setString('profilePicture', user.photoURL.toString());
+      } else {
+          await preferences!.setString('id', documents[0]['id']);
+          await preferences!.setString('username', documents[0]['username']);
+          await preferences!.setString('profilePicture', documents[0]['profilePicture']);
+      }
+
+      Fluttertoast.showToast(msg: 'Login was successful');
+      setState(() {
+        loading = false;
+      });
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomePage()));
+    } else {
+      Fluttertoast.showToast(msg: 'Login failed');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: Text("Login", style: TextStyle(color: Colors.red.shade900),),
+        elevation: 0.1,
+      ),
+      body: Stack(
+        children: [
+          Visibility(
+            visible: loading ? true : false,
+            child: Center(
+              child: Container(
+                alignment: Alignment.center,
+                color: Colors.white.withOpacity(0.9),
+                child: CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.red),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: Container(
+        child:  Center(
+          child: TextButton(
+            onPressed: () {
+              handleSignIn();
+            },
+            child: Text('Sign in with google', style: TextStyle(color: Colors.red),),
+          ),
+        ),
+      ),
+    );
   }
 }
-
